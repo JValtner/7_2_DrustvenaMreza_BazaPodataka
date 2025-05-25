da7bce207fe1859442404e7496984b2d22816db5
Josip Valtner <josipvaltner@gmail.com>
2025-05-25 16:10:07 +0200

    Ubaceni GetAll i GetById kontroleri

--- 7_2_drustvena_mreza/Controllers/GrupaController.cs
+++ 7_2_drustvena_mreza/Controllers/GrupaController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using _6_1_drustvena_mreza.DOMEN;
 using _6_1_drustvena_mreza.REPO;
+using _7_2_drustvena_mreza.REPO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
@@ -13,12 +14,13 @@ namespace _6_1_drustvena_mreza.Controllers
     {
         private GrupaRepo grupaRepo = new GrupaRepo();
         private KorisnikRepo korisnikRepo = new KorisnikRepo();
+        private GroupDbRepo groupDbRepo = new GroupDbRepo();
 
         // Get api/groups
         [HttpGet]
         public ActionResult<List<Grupa>> GetAll()
         {
-            List<Grupa> grupe = GetAllFromDatabase();
+            List<Grupa> grupe = groupDbRepo.GetAll();
             if (grupe == null) 
             { 
                 return NotFound("Ne postoji ni jedna grupa");
@@ -26,18 +28,16 @@ namespace _6_1_drustvena_mreza.Controllers
             return Ok(grupe);
         }
         [HttpGet("{grupaId}")]
-        public ActionResult<Korisnik> NadjiKorisnikaGrupeId(int grupaId)
-        {
-            if (!GrupaRepo.grupaRepo.ContainsKey(grupaId))
+        public ActionResult<Grupa> GetGroup(int grupaId)
+        {   
+            Grupa grupa = groupDbRepo.GetGroup(grupaId);
+            if (grupa ==null)
             {
                 return NotFound("Takva grupa ne postoji");
             }
-            List<Korisnik> listakorisnikaGrupe = korisnikRepo.NadjiKorisnike(grupaId);
-
-            return Ok(listakorisnikaGrupe);
+            return Ok(grupa);
         }
-
-
+        
         // POST api/groups
         [HttpPost]
         public ActionResult<Grupa> Create([FromBody] Grupa newGrupa)
@@ -46,9 +46,7 @@ namespace _6_1_drustvena_mreza.Controllers
             {
                 return BadRequest();
             }
-            newGrupa.Id = SracunajNoviId(GrupaRepo.grupaRepo.Keys.ToList());
-            GrupaRepo.grupaRepo[newGrupa.Id] = newGrupa;
-            grupaRepo.Sacuvaj();
+            groupDbRepo.NewGroup(newGrupa);
 
             return Ok(newGrupa);
         }
@@ -64,66 +62,6 @@ namespace _6_1_drustvena_mreza.Controllers
             grupaRepo.Sacuvaj();
 
             return NoContent();
-        }
-
-        private int SracunajNoviId(List<int> identifikatori)
-        {
-            int maxId = 0;
-            foreach (int id in identifikatori)
-            {
-                if (id > maxId)
-                {
-                    maxId = id;
-                }
-            }
-
-            return maxId + 1;
-        } 
-        private List<Grupa> GetAllFromDatabase()
-        {
-            List<Grupa> listaGrupa = new List<Grupa>();
-            try
-            {
-                using SqliteConnection connection = new SqliteConnection("Data Source=DATABASE/DrustveneMrezeDB.db");
-                connection.Open();
-
-                string query = "SELECT Id, Name, CreationDate FROM Groups";
-                using SqliteCommand command = new SqliteCommand(query, connection);
-
-                using SqliteDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    int id = Convert.ToInt32(reader["Id"]);
-                    string ime = reader["Name"].ToString();
-
-                    DateTime datumOsnivanja = DateTime.ParseExact(reader["CreationDate"].ToString(),"yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture
-);
-                    Grupa g = new Grupa(id, ime, datumOsnivanja);
-                    listaGrupa.Add(g);
-                }
-                return listaGrupa;
-            }
-            catch (SqliteException ex)
-            {
-                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
-                return null;
-            }
-            catch (FormatException ex)
-            {
-                return null;
-                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
-            }
-            catch (InvalidOperationException ex)
-            {
-                return null;
-                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                return null;
-                Console.WriteLine($"Neočekivana greška: {ex.Message}");
-            }
-
         }
     }
 }
---
+++ 7_2_drustvena_mreza/REPO/GroupDbRepo.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using _6_1_drustvena_mreza.DOMEN;
+using Microsoft.Data.Sqlite;
+namespace _7_2_drustvena_mreza.REPO
+{
+    public class GroupDbRepo
+    {
+        public List<Grupa> GetAll()
+        {
+            List<Grupa> listaGrupa = new List<Grupa>();
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection("Data Source=DATABASE/DrustveneMrezeDB.db");
+                connection.Open();
+
+                string query = "SELECT Id, Name, CreationDate FROM Groups";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                using SqliteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["Id"]);
+                    string ime = reader["Name"].ToString();
+
+                    DateTime datumOsnivanja = DateTime.ParseExact(reader["CreationDate"].ToString(), "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture
+);
+                    Grupa g = new Grupa(id, ime, datumOsnivanja);
+                    listaGrupa.Add(g);
+                }
+                return listaGrupa;
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                return null;
+            }
+            catch (FormatException ex)
+            {
+                return null;
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return null;
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return null;
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+
+        }
+        public Grupa GetGroup(int grupaId)
+        {
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection("Data Source=DATABASE/DrustveneMrezeDB.db");
+                connection.Open();
+
+                string query = "SELECT Id, Name, CreationDate FROM Groups WHERE Id=@Id";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", grupaId);
+                using SqliteDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["Id"]);
+                    string ime = reader["Name"].ToString();
+                    DateTime datumOsnivanja = DateTime.ParseExact(reader["CreationDate"].ToString(), "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                    Grupa grupa = new Grupa(id, ime, datumOsnivanja);
+                    return grupa;
+                }
+                return null;
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                return null;
+            }
+            catch (FormatException ex)
+            {
+                return null;
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return null;
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return null;
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+
+        }
+        public void NewGroup(Grupa grupa)
+        {
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection("Data Source=DATABASE/DrustveneMrezeDB.db");
+                connection.Open();
+
+                string query = "INSERT INTO Groups (Name,CreationDate) VALUES (@Ime, @DatumOsnivanja)";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+                command.Parameters.AddWithValue("@Ime", grupa.Ime);
+                command.Parameters.AddWithValue("@Id", grupa.DatumOsnivanja.ToString("yyyy-MM-dd"));
+                using SqliteDataReader reader = command.ExecuteReader();
+                
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+
+        }
+    }
+}
